96333fd3e8d5d1e3fec0da6494d83ceb1691ab3d
Manish <manishagarwal428728@gmail.com>
2024-12-16 18:04:21 +0530

    Initial Commit

---
+++ foodApp/foodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodAppApp.swift
+//  foodApp
+//
+//  Created by Manish Agarwal on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct foodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ foodAppTests/foodAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  foodAppTests.swift
+//  foodAppTests
+//
+//  Created by Manish Agarwal on 16/12/24.
+//
+
+import Testing
+@testable import foodApp
+
+struct foodAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
